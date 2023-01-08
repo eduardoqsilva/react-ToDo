93194376ec1020f38e57a73e335d996781aed0e9
eduardoqsilva <eduadsilva@outlook.com>
2023-01-08 18:23:04 -0300

    move and create a new components to folder components

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import TodoForm from "./TodoForm";
-import Item from './item'
-import List from "./List";
+import TodoForm from "./components/TodoForm";
+import Item from './components/Item'
+import List from "./components/List";
 
 import './Todo.css'
 
@@ -18,12 +18,21 @@ function Todo() {
     let filteredItens = itens.filter(it => it.id != item.id)
     setItens(filteredItens)
   }
+  function onDone(item){
+    let updatedItens = itens.map(it => {
+      if(it.id === item.id){
+        it.done = !it.done
+      }
+      return it
+    })
+    setItens(updatedItens)
+  }
 
   return (
     <div className="container">
       <h1>Todo</h1>
         <TodoForm onAddItem={onAddItem}></TodoForm>
-        <List onItemDeleted={onItemDeleted} itens={itens}></List>
+        <List onDone={onDone} onItemDeleted={onItemDeleted} itens={itens}></List>
     </div>
   )
 }
